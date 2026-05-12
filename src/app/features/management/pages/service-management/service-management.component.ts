80d740368d581e082904f7c59abf222114eb3336
Pedro Gabriel <pedrogabriellima@gmail.com>
2026-05-12 15:48:39 -0300

    feat(service): add PageHeaderComponent to service management and registration pages

--- src/app/features/management/pages/service-management/service-management.component.ts
+++ src/app/features/management/pages/service-management/service-management.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { ServiceManagementTableComponent } from './components/patient-management-table/service-management-table.component';
+import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 
 @Component({
   selector: 'app-service-management',
   standalone: true,
-  imports: [ServiceManagementTableComponent],
+  imports: [
+    ServiceManagementTableComponent, 
+    PageHeaderComponent
+  ],
   templateUrl: './service-management.component.html',
   styleUrl: './service-management.component.scss'
 })
